946c6717d3fff998581303dbeb7f9915e8362a2c
danifrakanza@gmail.com <danifrakanza@gmail.com>
2024-11-11 22:37:43 +0700

    menambahka validasi data untuk memastikan input lengkap dan email valid

--- fast.php
+++ fast.php
@@ -3,6 +3,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $name = trim($_post['name']);
     $email = trim($_post['email']);
     $message = trim($_post['message']);
-    
+
+    if (empty($name) || empty($email) ||empty($message) ||!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        http_response_code(400);
+        echo 'data tidak valid.';
+        exit;
+    }
 } 
 ?>
