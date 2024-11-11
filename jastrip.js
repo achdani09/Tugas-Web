2a09e1964529c5a8695612159ae96fe46e4397ff
danifrakanza@gmail.com <danifrakanza@gmail.com>
2024-11-11 21:14:19 +0700

    membuat objek formdata dari elemen form untuk pengiriman data

--- jastrip.js
+++ jastrip.js
@@ -1,7 +1,10 @@
 document.getElementById('contact-form').addEventListener('submit', async function (e) {
     e.preventDefault();
+
     const formstatus = document.getElementById('form-status');
     formstatus.innerText = 'mengirim...';
 
+    const formData = new formData(this);
+    
     
 })
